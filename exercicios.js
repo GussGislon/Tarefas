24abdedab0b72b9d83b425d8b893982c092fc41c
gustavo adolfo kletke gislon <115839132+GussGislon@users.noreply.github.com>
2023-05-09 19:35:06 -0300

    atualizado
    atualizado

--- exercicios.js
+++ exercicios.js
@@ -8,20 +8,7 @@
  */
 function soma(a, b) {
   // @todo implementar retorno da operação aqui
-let a = 10;
-let b = 15;
-
-let soma = a + b;
-
-console.log('Resultado da operação: ', soma);
-
-function soma(a, b) {
-  return a + b;
-}
-
-let valorsoma = soma(10, 15);
-
-console.log(valorsoma);
+return a + b;
 }
 
 /**
@@ -32,20 +19,7 @@ console.log(valorsoma);
  */
 function multiplicacao(a, b) {
   // @todo implementar retorno da operação aqui
-let a = 10;
-let b = 15;
-
-let multiplicacao = a * b;
-
-console.log('Resultado da operação: ', multiplicacao);
-
-function multiplicacao(a, b) {
-  return a * b;
-}
-
-let valormulti = multiplicacao(10, 15);
-
-console.log(valormulti);
+return a * b;
 }
 
 /**
@@ -56,20 +30,7 @@ console.log(valormulti);
  */
 function divisao(a, b) {
   // @todo implementar retorno da operação aqui
-let a = 10;
-let b = 15;
-
-let divisao = a / b;
-
-console.log('Resultado da operação: ', divisao);
-
-function divisao(a, b) {
-  return a / b;
-}
-
-let valordivisao = divisao(10, 15);
-
-console.log(valordivisao);
+return a / b;
 }
 
 /**
@@ -80,20 +41,7 @@ console.log(valordivisao);
  */
 function subtracao(a, b) {
   // @todo implementar retorno da operação aqui
-let a = 10;
-let b = 15;
-
-let subtracao = a - b;
-
-console.log('Resultado da operação: ', subtracao);
-
-function subtracao(a, b) {
-  return a - b;
-}
-
-let valorsubtracao = subtracao(10, 15);
-
-console.log(valorsubtracao);
+return a - b;
 }
 
 /**
@@ -104,20 +52,7 @@ console.log(valorsubtracao);
  */
 function modulo(a, b) {
   // @todo implementar retorno da operação aqui
-let a = 10;
-let b = 15;
-
-let modulo = a % b;
-
-console.log('Resultado da operação: ', modulo);
-
-function modulo(a, b) {
-  return a % b;
-}
-
-let valormodulo = modulo(10, 15);
-
-console.log(valormodulo);
+return a % b;
 }
 
 /**
@@ -128,23 +63,13 @@ console.log(valormodulo);
  */
 function max(a, b) {
   // @todo implementar retorno da operação aqui
-let a = 10;
-let b = 15;
 
-let max = a > b;
-
-console.log('Resultado da operação: ', max);
-
-function max(a, b) {
-  if(a > b)
-  return a;
-  else return b;
+  if(a > b) {
+  return a;}
+  else {
+    return b;}
 }
 
-let valormaior = max(10, 15);
-
-console.log(valormaior)
-}
 
 // -------------------------------------
 
@@ -156,24 +81,7 @@ console.log(valormaior)
  */
 function strConcat(a, b) {
   // @todo implementar retorno da operação aqui
-let str1 = "Gustavo";
-let str2 = "Gislon";
-
-let concat = str1 + ' ' + str2;
-
-console.log('Concat: ', concat);
-
-// OU...
-
-let concat2 = `${str1} ${str2}`;
-console.log('Concat2: ', concat2);
-
-function strConcat(str1, str2){
-  return str1 + ' ' + str2;
-}
-
-let string1 = strConcat(Gustavo, Gislon);
-console.log(string1);
+return `${a} ${b}`;
 }
 
 // --------------------------------------
@@ -189,16 +97,14 @@ console.log(string1);
  */
 function retornaNumero(numero) {
   // @todo implementar retorno da operação aqui
-  let num = 15
-
-if (num < 10) {
-  console.log("O numero é menor que 10.");
+if (numero > 10) {
+  return "Maior que 10" 
 }
-else if (num = 10) {
-  console.log("O numero é igual a 10.");
+else if (numero == 10) {
+  return "Igual a 10"
 }
-else (num > 10) {
-  console.log("O numero é maior que 10.");
+else {
+  return "Menor que 10"
 }
 }
 
@@ -211,13 +117,12 @@ else (num > 10) {
  */
 function ehPositivo(num) {
   // @todo implementar retorno da operação aqui
-  let num = 5
-
+ 
   if (num > 0) {
-    console.log("True");
+    return true;
   }
   else (num < 0) {
-    console.log("False")
+    return false;
   }
 }
 
